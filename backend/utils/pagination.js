072ca217d45c952437e31a49e44b2e4ff6c6d7a9
Danilo Scano <daniloscano@gmail.com>
2025-06-13 02:47:30 +0200

    added pagination utility

---
+++ backend/utils/pagination.js
@@ -0,0 +1,25 @@
+class Pagination {
+    constructor(model) {
+        this.model = model
+    }
+
+    async getPaginatedData(page, pageSize, filter = {}, sort = {}, populateFields = []) {
+        const totalResults = await this.model.countDocuments(filter)
+        const totalPages = Math.ceil( totalResults / pageSize )
+        const data = await this.model.find(filter)
+            .sort(sort)
+            .limit(pageSize)
+            .skip( (page - 1) * pageSize)
+            .populate(populateFields)
+
+        return {
+            totalResults,
+            totalPages,
+            page,
+            pageSize,
+            data
+        }
+    }
+}
+
+module.exports = Pagination
