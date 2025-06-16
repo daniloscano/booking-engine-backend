071163d4f1c3e5bddccfa9f331d7a29912329162
Danilo Scano <daniloscano@gmail.com>
2025-06-17 00:47:23 +0200

    added user schema

---
+++ models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            required: true,
+            default: 'user',
+            enum: [ 'admin', 'manager', 'operator', 'user' ]
+        },
+        guestId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'guest'
+        }
+    }, { timestamps: true, strict: true }
+)
+
+UserSchema.pre('save', async function(next) {
+    const user = this
+
+    if (!user.isModified('password')) {
+        return next()
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+
+        next()
+    } catch (err) {
+        next(err)
+    }
+})
+
+UserSchema.pre('updateOne', async function(next) {
+    const update = this.getUpdate()
+
+    if (!update || !update.password) {
+        return next()
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        update.password = await bcrypt.hash(update.password, salt)
+        this.setUpdate(update)
+
+        next()
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = mongoose.model('user', UserSchema, 'users')
