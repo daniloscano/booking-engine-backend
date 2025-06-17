7af4b264bbbb483dd9dc0ff75937a840f57938e9
Danilo Scano <daniloscano@gmail.com>
2025-06-17 02:27:38 +0200

    updated user schema on middleware pre findOneAndUpdate

--- models/user.js
+++ models/user.js
@@ -47,7 +47,7 @@ UserSchema.pre('save', async function(next) {
     }
 })
 
-UserSchema.pre('updateOne', async function(next) {
+UserSchema.pre('findOneAndUpdate', async function(next) {
     const update = this.getUpdate()
 
     if (!update || !update.password) {
